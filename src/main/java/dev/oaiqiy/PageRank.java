83e548d210eb764c99a99caec2b04675b048700e
oaiqiy <1320371940@qq.com>
2022-06-06 14:12:50 +0800

    feat: package to submit in spark cluster

--- src/main/java/dev/oaiqiy/PageRank.java
+++ src/main/java/dev/oaiqiy/PageRank.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Iterables;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
@@ -17,25 +18,30 @@ public class PageRank {
     private static final int count = 10;
 
     public static void main(String[] args) throws IOException {
-        SparkConf conf = new SparkConf().setAppName("PageRank1").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("PageRank1");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        String rankPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\dataset\\pr.txt";
-        String linkPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\dataset\\web-Google.txt";
-        String outputPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\output.txt";
+//        String rankPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\dataset\\pr.txt";
+//        String linkPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\dataset\\web-Google.txt";
+//        String outputPath = "F:\\javaProject\\spark-pagerank\\src\\main\\resources\\output.txt";
 
-        JavaPairRDD<String,Iterable<String>> links = sc.textFile(linkPath).mapToPair(new PairFunction<String, String, String>() {
+        String linkPath = args[0];
+        String outputPath = args[1];
+
+        JavaPairRDD<String,String> source = sc.textFile(linkPath).mapToPair(new PairFunction<String, String, String>() {
             @Override
             public Tuple2<String, String> call(String s) throws Exception {
                 String[] kv = s.split("\\s+|\\t");
                 return new Tuple2<>(kv[0],kv[1]);
             }
-        }).distinct().groupByKey().cache();
+        });
 
-        JavaPairRDD<String,Double> ranks = links.keys().mapToPair(new PairFunction<String, String, Double>() {
+        JavaPairRDD<String,Iterable<String>> links = source.distinct().groupByKey().cache();
+
+        JavaPairRDD<String,Double> ranks = source.keys().union(source.values()).distinct().mapToPair(new PairFunction<String, String, Double>() {
             @Override
             public Tuple2<String, Double> call(String s) throws Exception {
-                return new Tuple2<String, Double>(s,1.0);
+                return new Tuple2<>(s, 1.0);
             }
         });
 
@@ -56,23 +62,51 @@ public class PageRank {
 
         for(int i = 0;i < count;i++){
 
-            JavaPairRDD<String, Double> contributions = links.join(ranks).values().flatMapToPair(
-                    (PairFlatMapFunction<Tuple2<Iterable<String>, Double>, String, Double>) tuple2 -> {
+//            JavaPairRDD<String, Double> contributions = links.join(ranks).values().flatMapToPair(
+//                    (PairFlatMapFunction<Tuple2<Iterable<String>, Double>, String, Double>) tuple2 -> {
+//
+//                        int targetCount = Iterables.size(tuple2._1);
+//
+//                        List<Tuple2<String,Double>> result = new ArrayList<>(targetCount);
+//                        double v = tuple2._2 / targetCount;
+//                        for(String s : tuple2._1)
+//                            result.add(new Tuple2<>(s,v));
+//
+//                        return result.iterator();
+//            });
 
-                        int targetCount = Iterables.size(tuple2._1);
+            JavaPairRDD<String, Double> contributions = links.rightOuterJoin(ranks).flatMapToPair(new PairFlatMapFunction<Tuple2<String, Tuple2<Optional<Iterable<String>>, Double>>, String, Double>() {
+                @Override
+                public Iterator<Tuple2<String, Double>> call(Tuple2<String, Tuple2<Optional<Iterable<String>>, Double>> tuple) throws Exception {
+                    String k = tuple._1;
+
+                    Tuple2<Optional<Iterable<String>>, Double> tuple2 = tuple._2;
+                    if(tuple2._1.isPresent()){
+                        Iterable<String> iterable = tuple2._1.get();
+
+                        int targetCount = Iterables.size(iterable);
 
                         List<Tuple2<String,Double>> result = new ArrayList<>(targetCount);
                         double v = tuple2._2 / targetCount;
-                        for(String s : tuple2._1)
+                        for(String s : iterable)
                             result.add(new Tuple2<>(s,v));
 
+                        result.add(new Tuple2<>(k,0.0));
                         return result.iterator();
+                    }else{
+                        List<Tuple2<String,Double>> result = new ArrayList<>(1);
+                        result.add(new Tuple2<>(k,0.0));
+                        return result.iterator();
+                    }
+
+                }
             });
 
             ranks = contributions.reduceByKey((Function2<Double, Double, Double>) Double::sum)
                     .mapValues(sum -> 0.15 + sum * 0.85);
 
         }
+
 
         List<Tuple2<String,Double>> output = ranks.collect();
 
